639e929379c62140515fb640bc043f016610383b
Stefan Miljevic <77570817+steve958@users.noreply.github.com>
2023-09-26 12:30:20 +0200

    special offer update

--- src/components/SpecialOfferComponent.tsx
+++ src/components/SpecialOfferComponent.tsx
@@ -1,48 +1,99 @@
-import './SpecialOfferComponent.css';
+import "./SpecialOfferComponent.css";
+import slika1 from "../assets/specialoffer/srebrnaplaza.jpg";
+import slika2 from "../assets/specialoffer/splitriva.jpg";
+import slika3 from "../assets/specialoffer/kucakut.jpg";
 
 export default function SpecialOfferComponent() {
     return (
-        <div className="container" style={{ marginTop: '2%' }} >
-            <h3 style={{ textAlign: 'center', marginBottom: '2%' }}>SPECIAL OFFER</h3>
+        <div className="container" style={{ marginTop: "2%" }}>
+            <div className="divider">SPECIAL OFFER</div>
             <div className="card-group vgr-cards">
-                <div className="card">
-                    <div className="card-img-body">
-                        <img className="card-img" src="https://picsum.photos/500/230" alt="Card image cap" />
+                <div className="card" style={{ padding: "10px" }}>
+                    <div className="card-img-body" style={{ height: "370px" }}>
+                        <img className="card-img" src={slika1} alt="Card image cap" />
                     </div>
                     <div className="card-body">
-                        <h4 className="card-title">Card title</h4>
+                        <h4 className="card-title">NEW HOUSE - VIS RUKAVAC</h4>
                         <p className="card-text">
-                            This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer. This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+                            Newly built 3-room house with move-in deadline Summer 2023.
                         </p>
-                        <a href="#" className="btn btn-outline-primary">Primary</a>
+                        <p>View of the sea and nature.</p>
+                        <p>A quiet neighborhood full of greenery.</p>
+                        <a href="#" className="btn btn-outline-secondary">
+                            More details
+                        </a>
+                        <div className="icon_container_right">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="80"
+                                height="80"
+                                fill="#C99B11"
+                                className="bi bi-house-door"
+                                viewBox="0 0 16 16"
+                            >
+                                <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146ZM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5Z" />
+                            </svg>
+                        </div>
                     </div>
                 </div>
-                <div className="card">
-                    <div className="card-img-body">
-                        <img className="card-img" src="https://picsum.photos/400/200" alt="Card image cap" />
+                <div className="card" style={{ padding: "10px" }}>
+                    <div className="card-img-body" style={{ height: "370px" }}>
+                        <img className="card-img" src={slika2} alt="Card image cap" />
                     </div>
                     <div className="card-body">
-                        <h4 className="card-title">Card title</h4>
+                        <h4 className="card-title">APARTMENT - SPLIT</h4>
                         <p className="card-text">
-                            This card has supporting text below as a natural lead-in to additional content. This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer. This card has supporting text below as a natural lead-in to additional content.
+                            Completely arranged and renovated apartment in the very core of
+                            the city of Split.
                         </p>
-                        <a href="#" className="btn btn-outline-primary">Primary</a>
+                        <p>Every room and room has windows and natural ventilation.</p>
+                        <p>A perfect apartment as well as an investment!</p>
+                        <a href="#" className="btn btn-outline-secondary">
+                            More details
+                        </a>
+                        <div className="icon_container_left">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="80"
+                                height="80"
+                                fill="#C99B11"
+                                className="bi bi-buildings"
+                                viewBox="0 0 16 16"
+                            >
+                                <path d="M14.763.075A.5.5 0 0 1 15 .5v15a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5V14h-1v1.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V10a.5.5 0 0 1 .342-.474L6 7.64V4.5a.5.5 0 0 1 .276-.447l8-4a.5.5 0 0 1 .487.022ZM6 8.694 1 10.36V15h5V8.694ZM7 15h2v-1.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5V15h2V1.309l-7 3.5V15Z" />
+                                <path d="M2 11h1v1H2v-1Zm2 0h1v1H4v-1Zm-2 2h1v1H2v-1Zm2 0h1v1H4v-1Zm4-4h1v1H8V9Zm2 0h1v1h-1V9Zm-2 2h1v1H8v-1Zm2 0h1v1h-1v-1Zm2-2h1v1h-1V9Zm0 2h1v1h-1v-1ZM8 7h1v1H8V7Zm2 0h1v1h-1V7Zm2 0h1v1h-1V7ZM8 5h1v1H8V5Zm2 0h1v1h-1V5Zm2 0h1v1h-1V5Zm0-2h1v1h-1V3Z" />
+                            </svg>
+                        </div>
                     </div>
                 </div>
-                <div className="card">
-                    <div className="card-img-body">
-                        <img className="card-img" src="https://picsum.photos/450/250" alt="Card image cap" />
+                <div className="card" style={{ padding: "10px" }}>
+                    <div className="card-img-body" style={{ height: "370px" }}>
+                        <img className="card-img" src={slika3} alt="Card image cap" />
                     </div>
                     <div className="card-body">
-                        <h4 className="card-title">Card title</h4>
+                        <h4 className="card-title">NEW HOUSE - KUT RUKAVAC</h4>
                         <p className="card-text">
-                            This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action. This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+                            Four-room house witouth previous owners.
                         </p>
-                        <a href="#" className="btn btn-outline-primary">Primary</a>
+                        <p>Square footage 130m². The possibility of a swimming pool.</p>
+                        <a href="#" className="btn btn-outline-secondary">
+                            More details
+                        </a>
+                        <div className="icon_container_right">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="80"
+                                height="80"
+                                fill="#C99B11"
+                                className="bi bi-house-door"
+                                viewBox="0 0 16 16"
+                            >
+                                <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146ZM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5Z" />
+                            </svg>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     );
 }
-
