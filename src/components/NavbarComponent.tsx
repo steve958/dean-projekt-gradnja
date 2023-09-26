cb48e49ff0eee9217f77613008cf7578af95d3b9
Stefan Miljevic <77570817+steve958@users.noreply.github.com>
2023-09-26 12:42:00 +0200

    offer update

--- src/components/NavbarComponent.tsx
+++ src/components/NavbarComponent.tsx
@@ -8,7 +8,7 @@ export default function NavbarComponent() {
                 <Navbar.Brand href="/" style={{ fontWeight: '700' }}>Dean Projekt Gradnja</Navbar.Brand>
                 <Navbar.Toggle aria-controls="navbarNav" />
                 <Navbar.Collapse id="navbarNav">
-                    <Nav className="ml-auto">
+                    <Nav className="ml-auto" style={{ fontSize: '18px' }}>
                         <Nav.Link href="/">Home</Nav.Link>
                         <Nav.Link href="/about">About</Nav.Link>
                         <Nav.Link href="/contact">Contact</Nav.Link>
