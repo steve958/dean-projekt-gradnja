dbf115d9bbc56d9a7db7805adc167f132a320a3a
Stefan Miljevic <77570817+steve958@users.noreply.github.com>
2023-10-02 09:20:49 +0200

    contact page update

--- src/components/ContactComponent.tsx
+++ src/components/ContactComponent.tsx
@@ -1,6 +1,19 @@
+import { useState } from "react";
 import "./ContactComponent.css";
 
+interface Info {
+    home: boolean;
+    mail: boolean;
+    phone: boolean;
+}
+
 export default function ContactComponent() {
+    const [infoClicked, setInfoClicked] = useState<Info>({
+        home: false,
+        mail: false,
+        phone: false,
+    });
+
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const handleSubmit = (e: any) => {
         e.preventDefault();
@@ -15,14 +28,14 @@ export default function ContactComponent() {
                         <div className="card border-none rounded-2">
                             <div className="card-header p-0">
                                 <div
-                                    className="text-white text-center py-2"
+                                    className="text-white text-center p-4"
                                     style={{ backgroundColor: "#45526E" }}
                                 >
                                     <h3>Contact Us</h3>
                                     <p className="m-0">We will be glad to assist you</p>
                                 </div>
                             </div>
-                            <div className="card-body p-3">
+                            <div className="card-body p-5">
                                 <div className="form-group">
                                     <div className="input-group mb-2">
                                         <div className="input-group-prepend">
@@ -113,7 +126,17 @@ export default function ContactComponent() {
                 </div>
             </div>
             <div className="contact_icons_container">
-                <span className="contact_icon_wrapper" title="Donja Pastuša 23, Petrinja, HR">
+                <span
+                    className="contact_icon_wrapper"
+                    title="Donja Pastuša 23, Petrinja, HR"
+                    onClick={() =>
+                        setInfoClicked({
+                            home: !infoClicked.home,
+                            mail: false,
+                            phone: false,
+                        })
+                    }
+                >
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         width="50"
@@ -125,8 +148,21 @@ export default function ContactComponent() {
                         <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5ZM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5 5 5Z" />
                     </svg>
                     <p>Donja Pastuša 23, Petrinja, HR</p>
+                    {infoClicked.home && (
+                        <p className="info">Donja Pastuša 23, Petrinja, HR</p>
+                    )}
                 </span>
-                <span className="contact_icon_wrapper" title="info@deanprojektgradnja.com">
+                <span
+                    className="contact_icon_wrapper"
+                    title="info@deanprojektgradnja.com"
+                    onClick={() =>
+                        setInfoClicked({
+                            home: false,
+                            mail: !infoClicked.mail,
+                            phone: false,
+                        })
+                    }
+                >
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         width="50"
@@ -138,8 +174,21 @@ export default function ContactComponent() {
                         <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z" />
                     </svg>
                     <p>info@deanprojektgradnja.com</p>
+                    {infoClicked.mail && (
+                        <p className="info">info@deanprojektgradnja.com</p>
+                    )}
                 </span>
-                <span className="contact_icon_wrapper" title="+385 95 3466323">
+                <span
+                    className="contact_icon_wrapper"
+                    title="+385 95 3466323"
+                    onClick={() =>
+                        setInfoClicked({
+                            home: false,
+                            mail: false,
+                            phone: !infoClicked.phone,
+                        })
+                    }
+                >
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         width="40"
@@ -154,6 +203,7 @@ export default function ContactComponent() {
                         />
                     </svg>
                     <p>+385 95 3466323</p>
+                    {infoClicked.phone && <p className="info">+385 95 3466323</p>}
                 </span>
             </div>
         </div>
