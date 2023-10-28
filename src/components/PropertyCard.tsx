d9f7a5372964ce18e748c539113180fc0e820e60
Stefan Miljevic <77570817+steve958@users.noreply.github.com>
2023-10-28 11:03:49 +0200

    property card update

--- src/components/PropertyCard.tsx
+++ src/components/PropertyCard.tsx
@@ -1,4 +1,4 @@
-import { Card, Carousel, Modal, Form } from 'react-bootstrap';
+import { Carousel, Modal, Form } from 'react-bootstrap';
 import { useAppSelector } from "../app/hooks";
 import { RootState } from "../app/store";
 import { useEffect, useState } from "react";
@@ -30,39 +30,51 @@ const PropertyCard = () => {
 
     return (
         <>
-            <Card className="property_card-container">
-                <Carousel className="property_card-image">
-                    {property?.images?.map((image, index) => (
-                        <Carousel.Item key={index}>
-                            <img
-                                src={image}
-                                alt={`Slide ${index}`}
-                                style={{ height: "565px", objectFit: "contain" }}
-                            />
-                        </Carousel.Item>
-                    ))}
-                </Carousel>
-                <Card.Body className="property_card-body">
-                    <Card.Title style={{ fontSize: "25px" }}>
-                        {language === "English" ? property?.titleEng : property?.titleCro}
-                    </Card.Title>
-                    <Card.Text>
-                        {language === "English"
-                            ? property?.descriptionEng
-                            : property?.descriptionCro}
-                    </Card.Text>
-                    <Card.Text style={{ fontSize: "25px", fontWeight: '600' }}>
-                        {language === "English"
-                            ? `Price: ${property?.price && priceConverter(property?.price)}€`
-                            : `Cijena: ${property?.price && priceConverter(property?.price)}€`}
-                    </Card.Text>
-                    <span style={{ position: 'absolute', right: '10px', bottom: '30px' }}>
-                        <a className='btn btn-outline-secondary' onClick={handleSendMessage}>
-                            {language === "English" ? "Send Message" : "Pošaljite upit"}
-                        </a>
-                    </span>
-                </Card.Body>
-            </Card>
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-12">
+                        <div className="card property-card-container">
+                            <div className="property-card-image" style={{ backgroundColor: '#45526E' }}>
+                                <Carousel>
+                                    {property?.images?.map((image, index) => (
+                                        <Carousel.Item key={index}>
+                                            <img
+                                                src={image}
+                                                className="d-block w-100 shadow-lg rounded main-image"
+                                                alt={`Slide ${index}`}
+                                                style={{ maxHeight: "565px", objectFit: "contain" }}
+                                            />
+                                        </Carousel.Item>
+                                    ))}
+                                </Carousel>
+                            </div>
+                            <div className="card-body property-card-body">
+                                <h5 className="card-title" style={{ fontSize: "25px" }}>
+                                    {language === "English" ? property?.titleEng : property?.titleCro}
+                                </h5>
+                                <p className="card-text">
+                                    {language === "English"
+                                        ? property?.descriptionEng
+                                        : property?.descriptionCro}
+                                </p>
+                                <p style={{ fontSize: "25px", fontWeight: "600" }}>
+                                    {language === "English"
+                                        ? `Price: ${property?.price && priceConverter(property?.price)}€`
+                                        : `Cijena: ${property?.price && priceConverter(property?.price)}€`}
+                                </p>
+                                <span style={{ position: "absolute", right: "10px", bottom: "30px" }}>
+                                    <a
+                                        className="btn btn-outline-secondary"
+                                        onClick={handleSendMessage}
+                                    >
+                                        {language === "English" ? "Send Message" : "Pošaljite upit"}
+                                    </a>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <Modal show={showModal} onHide={handleCloseModal}>
                 <Modal.Header closeButton>
                     <Modal.Title>{language === "English" ? "Send Message" : "Pošaljite upit"}</Modal.Title>
