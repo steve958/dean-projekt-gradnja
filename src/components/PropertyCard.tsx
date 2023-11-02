386d1883fd0a622232555e6e58ca036c0cd15fbd
Stefan Miljevic <77570817+steve958@users.noreply.github.com>
2023-11-02 10:58:22 +0100

    maps updated

--- src/components/PropertyCard.tsx
+++ src/components/PropertyCard.tsx
@@ -4,44 +4,58 @@ import { RootState } from "../app/store";
 import { useEffect, useRef, useState } from "react";
 import { priceConverter } from "../helpers/priceconverter";
 import { useParams } from "react-router-dom";
+import { MapContainer, Marker, TileLayer } from "react-leaflet";
 import "./PropertyCard.css";
 import emailjs from "@emailjs/browser";
-import { Realestate, realestate } from "../data/realestateData";
+import { realestate } from "../data/realestateData";
+import "leaflet/dist/leaflet.css";
+import { LatLngTuple } from "leaflet";
+
+
+export interface Realestate {
+    id: number;
+    mainImage: string;
+    titleCro: string;
+    titleEng: string;
+    price: number;
+    images: string[];
+    status: string;
+    type: string;
+    descriptionCro: string;
+    descriptionEng: string;
+    summaryCro: string[];
+    summaryEng: string[];
+    coordinates: LatLngTuple;
+}
 
 const PropertyCard = () => {
     const language = useAppSelector((state: RootState) => state.language.value);
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const [property, setProperty] = useState<Realestate | null>(null);
     const [showModal, setShowModal] = useState(false);
-    const [messageSent, setMessageSent] = useState<boolean>(false);
-    const [messageError, setMessageError] = useState<boolean>(false);
+    const [showMap, setShowMap] = useState<boolean>(false);
     const { id } = useParams();
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const form = useRef<any>(null);
 
     useEffect(() => {
+        if (showMap) {
+            const attribution = document.querySelector(
+                ".leaflet-control-attribution"
+            ) as HTMLElement;
+            attribution.style.display = "none";
+        }
+    }, [showMap]);
+
+    useEffect(() => {
         const element = document.getElementsByClassName("card-body");
         element[0]?.scrollIntoView({ behavior: "smooth", block: "start" });
         const find = realestate?.find(
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
             (realestate: Realestate) => realestate.id.toString() === id
         );
         if (find) setProperty(find);
     }, [id]);
-
-    useEffect(() => {
-        if (messageSent) {
-            setTimeout(() => {
-                setMessageSent(false);
-            }, 3000);
-        }
-    }, [messageSent]);
-
-    useEffect(() => {
-        if (messageError) {
-            setTimeout(() => {
-                setMessageError(false);
-            }, 3000);
-        }
-    }, [messageError]);
 
     const handleSendMessage = () => {
         setShowModal(true);
@@ -63,12 +77,10 @@ const PropertyCard = () => {
             )
             .then(
                 (result) => {
-                    setMessageSent(true);
                     console.log(result.text);
-                    setShowModal(false)
+                    setShowModal(false);
                 },
                 (error) => {
-                    setMessageError(true);
                     console.log(error.text);
                 }
             );
@@ -117,6 +129,40 @@ const PropertyCard = () => {
                                     property?.summaryEng.map((summary: string) => {
                                         return <p key={Math.random()}>{summary}</p>;
                                     })}
+                                <p>Kontakt: +385 95 3466323</p>
+                                {!showMap && (
+                                    <span style={{ display: "flex", cursor: "pointer" }}>
+                                        <p onClick={() => setShowMap(true)}>Prikaži na mapi</p>
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            width="16"
+                                            height="16"
+                                            fill="currentColor"
+                                            className="bi bi-geo-alt-fill"
+                                            viewBox="0 0 16 16"
+                                        >
+                                            <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
+                                        </svg>
+                                    </span>
+                                )}
+                                {showMap && (
+                                    <div className="map_container">
+                                        <MapContainer
+                                            center={property!.coordinates}
+                                            zoom={15}
+                                            scrollWheelZoom={false}
+                                        >
+                                            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                                            <Marker position={property!.coordinates}></Marker>
+                                        </MapContainer>
+                                        <span
+                                            onClick={() => setShowMap(false)}
+                                            className="close_map"
+                                        >
+                                            X
+                                        </span>
+                                    </div>
+                                )}
                                 <p style={{ fontSize: "25px", fontWeight: "600" }}>
                                     {language === "English"
                                         ? `Price: ${property?.price && priceConverter(property?.price)
@@ -150,10 +196,13 @@ const PropertyCard = () => {
                         ref={form}
                     >
                         <Form.Group className="mb-3">
-                            <Form.Label name="property">
-                                Nekretnina
-                            </Form.Label>
-                            <Form.Control type='text' value={property?.titleCro} readOnly />
+                            <Form.Label>Nekretnina</Form.Label>
+                            <Form.Control
+                                type="text"
+                                name="property"
+                                value={property?.titleCro}
+                                readOnly
+                            />
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label>Name</Form.Label>
